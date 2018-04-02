dbd765f7fd2a52317b8995d290e1bbf9e40b4528
lizagilman <lizagilman@gmail.com>
2018-04-02 12:41:09 +0300

    adding eslint

--- static/src/js/components/router/router_index.js
+++ static/src/js/components/router/router_index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "../home/home_index";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../home/home_index';
 
 const AppRouter = () => (
   <Router>
