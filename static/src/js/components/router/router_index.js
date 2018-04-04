bb3eda61a3de3cbda33a3249096d5d1a568c56d1
Alisa <alisa.barsky@gmail.com>
2018-04-04 17:13:47 +0300

    wizard

--- static/src/js/components/router/router_index.js
+++ static/src/js/components/router/router_index.js
@@ -1,15 +1,13 @@
-
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../home/home_index";
 import Wizard from "../wizard/wizard";
 
-
 const AppRouter = () => (
   <Router>
     <div>
       <Route path="/" component={Home} />
-      <Route path="/wizard" component={Wizard} />
+      <Route exact path="/wizard" component={Wizard} />
       {/*
             <Route path="/some-url" component={component1} />
             <Route path="/url-with-param/:param" component={component2} />
--- static/src/js/components/wizard/wizard.js
+++ static/src/js/components/wizard/wizard.js
@@ -1,69 +1,61 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import ReactWizard from "react-bootstrap-wizard";
-import {Container, Row, Col} from "reactstrap";
-
-import {orange500, blue500} from 'material-ui/styles/colors';
+import { Container, Row, Col } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import TextField from "material-ui/TextField";
 
 import DatePickerExampleControlled from "./components/datePicker";
-import TextFieldExampleControlled from"./components/textField";
+import TextFieldExampleControlled from "./components/textField";
 
+const FirstStep = () => (
+  <div>
+    <TextField
+      hintText={"Tournament Name"}
+      floatingLabelText="Tournament Name"
+    />
 
+    <TextField hintText={"Address"} />
+  </div>
+);
 
-class FirstStep extends React.Component{
-
-    render(){
-        return (
-            <div>
-                <DatePickerExampleControlled/>
-                <TextFieldExampleControlled/>
-            </div>
-        )
-    }
+class SecondStep extends React.Component {
+  render() {
+    return <div>Hey from Second</div>;
+  }
 }
-class SecondStep extends React.Component{
-
-
-}
-class ThirdStep extends React.Component{
-    render(){
-        return (
-            <div>Hey from Third</div>
-        )
-    }
+class ThirdStep extends React.Component {
+  render() {
+    return <div>Hey Hey from Third</div>;
+  }
 }
 
 var steps = [
-    // this step hasn't got a isValidated() function, so it will be considered to be true
-    { stepName: 'Basic Info', component: FirstStep },
-    // this step will be validated to false
-    { stepName: 'Second', component: SecondStep },
-    // this step will never be reachable because of the seconds isValidated() steps function that will always return false
-    { stepName: 'Third', component: ThirdStep }
+  // this step hasn't got a isValidated() function, so it will be considered to be true
+  { stepName: "Basic Info", component: FirstStep },
+  // this step will be validated to false
+  { stepName: "Second", component: SecondStep },
+  // this step will never be reachable because of the seconds isValidated() steps function that will always return false
+  { stepName: "Third", component: ThirdStep }
 ];
 
-const Wizard = () => (
-
-
-    <Container fluid style={{marginTop: "15px"}}>
-        <Row>
-            <Col xs={12} md={6} className="mr-auto ml-auto">
-                <ReactWizard
-                    steps={steps}
-                    navSteps
-                    title="Tournament"
-                    subtitle=""
-                    headerTextCenter
-                    validate
-                    color="green"
-                />
-            </Col>
-        </Row>
+const Wizard = () => {
+  return (
+    <Container fluid style={{ marginTop: "15px" }}>
+      <Row>
+        <Col xs={12} md={6} className="mr-auto ml-auto">
+          <ReactWizard
+            steps={steps}
+            navSteps
+            title="Tournament"
+            subtitle=""
+            headerTextCenter
+            validate
+            color="green"
+          />
+        </Col>
+      </Row>
     </Container>
-);
+  );
+};
 
 export default Wizard;
-
-
-
