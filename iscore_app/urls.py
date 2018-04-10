45ec5b53bf3d1a45e97a1c267a942158934ecb28
Alisa <alisa.barsky@gmail.com>
2018-04-10 23:17:13 +0300

    draws

--- iscore_app/urls.py
+++ iscore_app/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from iscore_app.views import IndexView
 from iscore_app import views
+from iscore_app import utils
+from iscore_app import match
+
 
 router = routers.DefaultRouter()
 router.register(r'catagories', views.CatagoriesViewSet)
@@ -30,7 +33,7 @@ router.register(
 router.register(r'Coachs', views.CoachsViewSet)
 urlpatterns = [
     url(r'^api', include(router.urls)),
-    url(r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')),
-    url(r'', IndexView.as_view(), name='index'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+   # url(r'', IndexView.as_view(), name='index'),
+    url(r'', match.test1, name='test'),
 ]
