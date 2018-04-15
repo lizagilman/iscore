a9fe2d5ea5e676cc77e071b41d19bbf256a672d1
Alisa <alisa.barsky@gmail.com>
2018-04-15 16:34:23 +0300

    schedule page works

--- static/src/js/stores/matches_store.js
+++ static/src/js/stores/matches_store.js
@@ -1,5 +1,5 @@
 import { observable, action } from 'mobx';
-import { getAllMatchesApi } from '../api';
+import {getAllMatchesApi} from '../api';
 
 class MatchesStore {
 
@@ -7,43 +7,11 @@ class MatchesStore {
 
   @action
   fetchAllMatchs = () => {
-    let mockData=[{
-		"model": "iscore_app.matches",
-		"pk": 95,
-		"fields": {
-			"player2_score": 0,
-			"player1": "Alisa Suchman",
-			"player2": "Bob Bobski",
-			"winner": null,
-			"stage": "QF",
-			"time": null,
-			"index": 0,
-			"draws": 1
-		}
-	}, {
-		"model": "iscore_app.matches",
-		"pk": 109,
-		"fields": {
-			"player2_score": 0,
-			"player1": "Alisa Suchman",
-			"player2": "Kuki man",
-			"winner": null,
-			"stage": "QF",
-			"time": null,
-			"index": 0,
-			"draws": 1
-		}}];
-      this.allMatches = mockData;
-
-      //todo: Alisa should take the data from server !
-      /*
-      getAllMatchesApi().then((matchesJson) => {
+    getAllMatchesApi().then((matchesJson) => {
       this.allMatches = [matchesJson];
     });
-    */
   };
 
-  @action setTournament = () => {};
 }
 
 const matchesStore = new MatchesStore();
