ca1dd2fd45f6cca96195af85a34367e0c6673033
mik52023 <mik52023@walla.com>
2018-04-26 10:53:45 +0300

    create tournament generator

---
+++ iscore_app/tournament.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+import datetime
+from iscore_app.models import Matches, TournamentCategories, Entries, Tournaments
+from django.core import serializers
+
+
+def handle_generate_tournament(request):
+
+    tournament_info = request.GET['tournament']
+
+
+
+def generate_tournament(info):
+
+
+
+
+    return "success"
