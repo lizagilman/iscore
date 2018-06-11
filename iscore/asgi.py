61625e5bc63d367e08d5fae73551cdf2cbfe103a
mik52023 <mik52023@walla.com>
2018-06-11 11:35:28 +0300

    Update asgi.py

--- iscore/asgi.py
+++ iscore/asgi.py
@@ -8,5 +8,6 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iscore.settings")
+os.environ.setdefault("ASGI_THREADS", "3")
 django.setup()
 application = get_default_application()
